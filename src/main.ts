875494295c3dc35fa9fece93be6d644087be4813
Leonardus <LBotes@yellowtail.nl>
2023-02-10 15:38:01 +0200

    added firebase tools

--- src/main.ts
+++ src/main.ts
@@ -5,3 +5,27 @@ import { AppModule } from './app/app.module';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+  // Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getAuth } from "firebase/auth";
+import { FIREBASE } from './app/configs/keys';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: FIREBASE.ApiKey,
+  authDomain: FIREBASE.AuthDomain,
+  projectId: FIREBASE.ProjectId,
+  storageBucket: FIREBASE.StorageBucket,
+  messagingSenderId: FIREBASE.MessagingSenderId,
+  appId: FIREBASE.AppId,
+  databaseURL: FIREBASE.DatabaseURL
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const database = getDatabase(app);
