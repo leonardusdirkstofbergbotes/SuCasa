d02521c7c50d1cc659001707f1eab4d4e29fb59b
Leonardus <LBotes@yellowtail.nl>
2023-02-21 14:12:30 +0200

    added default routes for categories

--- server.ts
+++ server.ts
@@ -1,5 +1,6 @@
 import 'zone.js/node';
 
+import { FIREBASE } from './src/app/configs/keys';
 import { APP_BASE_HREF } from '@angular/common';
 import { ngExpressEngine } from '@nguniversal/express-engine';
 import * as express from 'express';
@@ -7,12 +8,43 @@ import { existsSync } from 'fs';
 import { join } from 'path';
 
 import { AppServerModule } from './src/main.server';
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { ref, set, get, child, remove } from 'firebase/database';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: FIREBASE.ApiKey,
+  authDomain: FIREBASE.AuthDomain,
+  projectId: FIREBASE.ProjectId,
+  storageBucket: FIREBASE.StorageBucket,
+  messagingSenderId: FIREBASE.MessagingSenderId,
+  appId: FIREBASE.AppId,
+  databaseURL: FIREBASE.DatabaseURL
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+const auth = getAuth(firebase);
+const database = getDatabase(firebase);
+const storage = getStorage(firebase);
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
   const server = express();
+  const cors = require('cors');
   const distFolder = join(process.cwd(), 'dist/Su-Casa/browser');
   const indexHtml = existsSync(join(distFolder, 'index.original.html')) ? 'index.original.html' : 'index';
+  const corsOptions = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200 // For legacy browser support
+  };
+  const db = getDatabase();
 
   // Our Universal express-engine (found @ https://github.com/angular/universal/tree/main/modules/express-engine)
   server.engine('html', ngExpressEngine({
@@ -21,6 +53,57 @@ export function app(): express.Express {
 
   server.set('view engine', 'html');
   server.set('views', distFolder);
+  server.use(cors(corsOptions));
+
+  // CATEGORIES END POINTS
+
+  // POST categories {formdata}
+  server.post('/api/categories', (req: any, res: any) => {
+    // save file first. 
+
+    const uniqueId = Math.floor(Date.now() + Math.random());
+    set(ref(db, 'categories/' + uniqueId), req.params.formData).then(() => {
+      res.status(200).send();
+    }).catch(error => {
+      res.status(400).send(error);
+    });
+  });
+
+  // GET category {categoryId}
+  server.get('/api/categories/:categoryId', (req: any, res: any) => {
+    get(child(ref(db), `categories/${req.params.categoryId}`)).then(data => {
+      return res.status(200).send(data);
+    }).catch(error => {
+      return res.status(400).send(error);
+    });
+  });
+
+  // PATCH categories {categoryId, formData}
+  server.patch('/api/categories/:categoryId', (req: any, res: any) => {
+    set(ref(db, `categories/${req.params.categoryId}`), req.params.formData).then(() => {
+      res.status(200).send();
+    }).catch(error => {
+      res.status(400).send(error);
+    })
+  });
+
+  // DELETE category {categoryId}
+  server.delete('api/categories/:categoryId', (req: any, res: any) => {
+    remove(ref(db, `categories/${req.params.categoryId}`)).then(() => {
+      res.status(200).send();
+    }).catch(error => {
+      res.status(400).send(error);
+    })
+  });
+
+  // GET all categories
+  server.get('/api/categories', (req:any, res: any) => {
+    get(child(ref(db), 'categories/')).then(data => {
+      return res.status(200).send(data);
+    }).catch(error => {
+      res.status(400).send(error);
+    });
+  });
 
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
